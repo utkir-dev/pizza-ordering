4f425b3ef168d71f05a56a531607bbe931d65633
U <umtokh80@gmail.com>
2023-06-20 00:02:20 +0500

    templates bo'laklarga ajratildi

--- projectPizza/urls.py
+++ projectPizza/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from projectPizza.views import index
+from projectPizza import views
 urlpatterns = [
-    path('', index),
+    path('', views.home,name='home'),
+    path('menu/', views.menu,name='menu'),
+    path('about/', views.about,name='about'),
+    path('services/', views.services,name='services'),
+    path('contact/', views.contact,name='contact'),
 ]
--- projectPizza/views.py
+++ projectPizza/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def home(request):
     return render(request,'index.html')
+
+def menu(request):
+    return render(request,'menu.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+
